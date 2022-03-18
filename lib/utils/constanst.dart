6c2d9424fcf3ceea710a111c0f45cc90216c3200
Jaramillo <julian.jaramillo@accenture.com>
2022-03-17 22:45:21 -0500

    First version scaffold and colors

---
+++ lib/utils/constanst.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor    = Color.fromRGBO(15,237,219,1);
+const Color kBackgroundColor = Color.fromRGBO(242, 242, 242,1);
+const Color kDangerColor     = Color.fromRGBO(249,77,30,1);
